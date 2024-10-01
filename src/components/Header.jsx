86eae885b379b45ce26fe926896da933ff568848
Aditya Gundigara <adityagundigara07@gmail.com>
2024-10-01 13:27:51 +0530

    Header Section Complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,20 @@ const Header = () => {
 
             {/* left side */}
 
-            <div className={'md:w-1/2  flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[30px]'}>
+            <div className={'md:w-1/2  flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-50px]'}>
                 <p className={'text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'}>Book Appointment <br /> With Trusted Doctors</p>
                 <div className={'flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'}>
-                    <img className={'w-24'} src={assets.group_profiles} alt="" />
+                    <img className={'w-16'} src={assets.group_profiles} alt="" />
                     <p>Simply browse through our extensive list of trusted doctors, <br className={'hidden sm:block'}/> schedule your appointmnet hassle-free.</p>
                 </div>
-                <a href="" className={'flex gap-2 items-center px-5 py-3 bg-white rounded-full text-gray-700'}>
-                    Book Appointment <img src={assets.arrow_icon} alt="" />
+                <a href="#speciality" className={'flex gap-2 items-center px-8 py-3 bg-white rounded-full text-gray-600 text-sm m-auto md:m-0  hover:scale-105 transition-all duration-300'}>
+                    Book Appointment <img className={'w-3'} src={assets.arrow_icon} alt="" />
                 </a>
             </div>
-
+ 
             {/* right side */}
-            <div className={'md:w-1/2 relative'}>
-                <img className={'w-full absolute bottom-0 h-auto rounded-lg'} src={assets.header_img} alt="" />
+            <div className={'md:w-1/2 relative '}>
+                <img className={'w-full md:absolute bottom-0 h-auto rounded-lg'} src={assets.header_img} alt="" />
             </div>
         </div>
     )
